515dda2e47c06aa93d4672b5733073e21140fda0
SilencedSign87 <diazvegabruno@hotmail.com>
2024-09-08 12:44:38 -0500

    Eliminación de archivos innecesarios

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-use App\Components\AdministradorComponent;
-use App\Components\LoginComponent;
 use App\Components\MainPage;
-use App\Components\TodosHeroes;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', MainPage::class);
-
-Route::get('/login',LoginComponent::class);
-
-Route::get('/admin', AdministradorComponent::class);
