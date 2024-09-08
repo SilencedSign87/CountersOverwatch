fb92a0200896d8106f87003e0fd87aa1ecd805f9
SilencedSign87 <diazvegabruno@hotmail.com>
2024-09-08 12:38:11 -0500

    Añadido icono en la nota de heroe

--- resources/views/main-page.blade.php
+++ resources/views/main-page.blade.php
@@ -546,6 +546,15 @@
                       <div class="cuerpo-contenido cuerpo-modal" wire:loading.remove wire:target='selectHero'>
                           {{-- Cuerpo --}}
                           <div class="nota-heroe">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 24 24"
+                                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  class="icon icon-tabler icons-tabler-outline icon-tabler-exclamation-circle">
+                                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                                  <path d="M12 9v4" />
+                                  <path d="M12 16v.01" />
+                              </svg>
                               <span>
                                   {{ $selectedHero->nota ?? 'llamen a dios' }}
                               </span>
