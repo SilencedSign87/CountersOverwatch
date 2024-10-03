c158d0b290e93effa50efd0ad925e71635902509
SilencedSign87 <diazvegabruno@hotmail.com>
2024-10-03 09:46:59 -0500

    Añadida funcion de cambiar de heroe seleccionado y render de las imagenes

--- resources/views/counters-edit.blade.php
+++ resources/views/counters-edit.blade.php
@@ -216,19 +216,27 @@
             width: 80px;
             height: 80px;
             transition: opacity 0.25s ease;
-            border:none;
+            border: none;
             user-select: none;
         }
 
         .imagen_heroe.active {
             opacity: 1;
             transition: all 0.25s;
-            border:none;
+            border: none;
         }
-        .imagen_heroe.selected{
+
+        .imagen_heroe.selected {
             background: rgba(0, 0, 0, 0.25);
             /* cambiar la luminosidad de la imagen */
             filter: brightness(0.5);
+        }
+
+        .imagen_hero_objetido {
+            background: rgba(0, 0, 0, 0.25);
+            width: 90px;
+            height: 90px;
+            border-radius: 5px;
         }
 
         .imagen_heroe:hover {
@@ -237,6 +245,79 @@
 
         .imagen_heroe:active {
             border: 1px solid white;
+        }
+
+        #contendor_heroes_add {
+            width: 85vw;
+            height: 100px;
+            display: flex;
+            gap: 2px;
+            justify-content: center;
+            align-items: flex-start;
+            overflow: hidden;
+            background: hsla(0, 0%, 100%, 0.50);
+            border-radius: 5px;
+            padding: 5px;
+        }
+
+        .boton_rol_footer {
+            width: 80px;
+            height: 80px;
+            border-radius: 5px;
+            background: rgba(255, 255, 255, 0.5);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: all 0.3s ease;
+        }
+
+        .boton_rol_footer:hover {
+            background: rgba(255, 255, 255, 0.75);
+        }
+
+        .boton_rol_footer:active {
+            background: rgba(255, 255, 255, 1);
+        }
+
+        .contenedor_heroes_footer {
+            display: flex;
+            overflow-x: auto;
+            width: 0;
+            white-space: nowrap;
+            transition: width 0.5s ease;
+            background: hsla(0, 0%, 100%, 0.75);
+        }
+
+        .contenedor_heroes_footer.active {
+            width: 80%;
+        }
+
+        .imagen_heroe_footer {
+            box-sizing: border-box;
+            width: 80px;
+            height: 80px;
+            opacity: 0;
+            transition: opacity 0.3s ease;
+        }
+
+        .imagen_heroe_footer.active {
+            opacity: 1;
+        }
+
+        #heroe-selected {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+        }
+
+        #hero_selected_counters {
+            box-sizing: border-box;
+            display: flex;
+            justify-content: flex-start;
+            align-items: flex-start;
+            min-height: 80px;
+            gap: 1px;
         }
     </style>
 
@@ -279,33 +360,156 @@
                 @foreach ($HeroesRol as $rol => $heroes)
                     <div class="boton_rol" data-rol="{{ $rol }}">
                         @if ($rol == 'tank')
-                            <img draggable="false" src="/logos/tankLogo.svg" alt="logo de tank" width="30" height="30">
+                            <img draggable="false" src="/logos/tankLogo.svg" alt="logo de tank" width="30"
+                                height="30">
                         @elseif($rol == 'dps')
-                            <img draggable="false" src="/logos/dpsLogo.svg" alt="logo de tank" width="30" height="30">
+                            <img draggable="false" src="/logos/dpsLogo.svg" alt="logo de tank" width="30"
+                                height="30">
                         @elseif($rol == 'supp')
-                            <img draggable="false" src="/logos/suppLogo.svg" alt="logo de tank" width="30" height="30">
+                            <img draggable="false" src="/logos/suppLogo.svg" alt="logo de tank" width="30"
+                                height="30">
                         @endif
                     </div>
-                    <div class="contenedor_heroes" data-rol="{{ $rol }}" >
+                    <div class="contenedor_heroes" data-rol="{{ $rol }}">
                         @foreach ($heroes as $hero)
-                            <img src="{{ $hero['img_path'] }}" alt="{{ $hero['nombre'] }}" class="imagen_heroe" draggable="false">
+                            <img src="{{ $hero['img_path'] }}" alt="{{ $hero['nombre'] }}" class="imagen_heroe"
+                                draggable="false" data-id="{{ $hero['id'] }}" data-rol="{{ $rol }}"
+                                onclick="selectHero('{{ $hero['id'] }}','{{ $hero['nombre'] }}' , '{{ $hero['img_path'] }}' , '{{ $rol }}')">
                         @endforeach
                     </div>
                 @endforeach
             </div>
             <div id="contendor_hero_counters">
+                <div id="heroe-selected">
 
+                </div>
+                <div id="hero_selected_counters">
+
+                </div>
             </div>
         </section>
         <footer>
             <div id="contendor_heroes_add">
-
+                @foreach ($HeroesRol as $rol => $heroes)
+                    <div class="boton_rol_footer" data-rol="{{ $rol }}">
+                        @if ($rol == 'tank')
+                            <img src="/logos/tankLogo.svg" alt="logo de tank" width="30" height="30">
+                        @elseif($rol == 'dps')
+                            <img src="/logos/dpsLogo.svg" alt="logo de dps" width="30" height="30">
+                        @elseif($rol == 'supp')
+                            <img src="/logos/suppLogo.svg" alt="logo de supp" width="30" height="30">
+                        @endif
+                    </div>
+                    <div class="contenedor_heroes_footer" data-rol="{{ $rol }}">
+                        @foreach ($heroes as $hero)
+                            <img src="{{ $hero['img_path'] }}" alt="{{ $hero['nombre'] }}" class="imagen_heroe_footer"
+                                draggable="false" data-id="{{ $hero['id'] }}" data-rol="{{ $rol }}"
+                                onclick="addCounter('{{ $hero['id'] }}' ,'{{ $hero['nombre'] }}' ,  '{{ $hero['img_path'] }}' , '{{ $rol }}')">
+                        @endforeach
+                    </div>
+                @endforeach
             </div>
-
         </footer>
     </article>
 
     <script>
+        // Datos de los counters
+        let dataCounters = [];
+        // id del heroe seleccionado
+        let selectedHeroId = null;
+
+        // Cambia el heroe seleccionado
+        function selectHero(id, nombre, img_path, rol) {
+            selectedHeroId = id;
+
+            // Buscar el héroe en la lista de héroes
+            let selectedHero = {
+                'id': id,
+                'nombre': nombre,
+                'img_path': img_path,
+                'rol': rol
+            }
+
+            let contenedorSelectedHero = document.getElementById('heroe-selected');
+            let countersContenedor = document.getElementById('hero_selected_counters');
+            
+            // Crear una imagen del heroe seleccionado
+            contenedorSelectedHero.innerHTML = `
+                <h3>${nombre}</h3>
+                <img src="${img_path}" alt="${nombre}" class="imagen_hero_objetido" draggable="false" data-id="${id}" data-rol="${rol}">
+            `;
+            // Limpiar las imagenes de counters existentes
+            countersContenedor.innerHTML = '';
+
+            // Buscar si el héroe ya tiene counters guardados en dataCounters
+            let existingHeroData = dataCounters.find(heroData => heroData.hero_id == selectedHeroId);
+
+            if (existingHeroData) {
+                // Si el héroe ya existe, cargar sus counters
+                countersSelectedHero = existingHeroData.counters;
+                // Actualizar la interfaz de usuario con los counters
+                
+                // añade las iamgenes de los counters
+                countersSelectedHero.forEach(counter => {
+                    countersContenedor.innerHTML += `
+                        <img src="${counter.img_path}" alt="${counter.nombre}" class="imagen_hero_objetido" title="${counter.nombre}" data-hero-id="${counter.hero_id}" data-index="${countersSelectedHero.indexOf(counter)}">
+                    `;
+                });
+
+            } else {
+                // Si el héroe es nuevo, inicializar un objeto para él en dataCounters
+                dataCounters.push({
+                    hero_id: selectedHeroId,
+                    img_path: selectedHero.img_path, // O la forma en que accedas a la ruta de la imagen
+                    counters: []
+                });
+                countersSelectedHero = []; // Reiniciar la lista de counters mostrados
+            }
+        }
+
+        // añade counter al heroe seleccionado
+        function addCounter(id, nombre, img_path, rol) {
+            if (selectedHeroId === null) {
+                // Manejar el caso en el que no se haya seleccionado un héroe objetivo
+                alert("Selecciona un héroe objetivo primero.");
+                return;
+            }
+
+            // Buscar el héroe que se quiere añadir como counter
+            let counterHero = {
+                'nombre': nombre,
+                'img_path': img_path,
+                'rol': rol
+            };
+
+            // Verificar si el counter ya existe para este héroe
+            let existingCounterIndex = countersSelectedHero.findIndex(counter => counter.hero_id == id);
+
+            if (existingCounterIndex === -1) {
+                // Si el counter no existe, agregarlo
+                countersSelectedHero.push({
+                    hero_id: id,
+                    img_path: counterHero.img_path // O la forma en que accedas a la ruta de la imagen
+                });
+
+                // Actualizar dataCounters con el nuevo counter
+                let heroDataIndex = dataCounters.findIndex(heroData => heroData.hero_id == selectedHeroId);
+                dataCounters[heroDataIndex].counters = countersSelectedHero;
+
+                // Actualizar la interfaz de usuario:
+
+                let countersContenedor = document.getElementById('hero_selected_counters');
+
+                countersContenedor.innerHTML += `
+                <img src="${counterHero.img_path}" alt="Counter" class="imagen_hero_objetido">
+                `;
+
+            } else {
+                // Si el counter ya existe:
+                // - No hacer nada
+            }
+        }
+
         document.addEventListener('DOMContentLoaded', function() {
             const btnSidebar = document.getElementById('btnToggle');
             const btnCerrar = document.getElementById('cerrarsesion');
@@ -363,6 +567,60 @@
                 });
             });
 
+            const botonesRolFooter = document.querySelectorAll('.boton_rol_footer');
+            const contenedoresHeroesFooter = document.querySelectorAll('.contenedor_heroes_footer');
+
+            botonesRolFooter.forEach(boton => {
+                boton.addEventListener('click', function() {
+                    // Obtener el rol del botón
+                    const rol = boton.dataset.rol;
+
+                    if (boton.classList.contains('seleccionado')) {
+                        // Colapsar el contenedor si está activo
+                        boton.classList.remove('seleccionado');
+                        contenedoresHeroesFooter.forEach(contenedor => {
+                            if (contenedor.dataset.rol === rol) {
+                                contenedor.classList.remove('active');
+                            }
+                        });
+                        return;
+                    }
+
+                    // Remover la clase .seleccionado de todos los botones
+                    botonesRolFooter.forEach(b => b.classList.remove('seleccionado'));
+
+                    // Añadir la clase .seleccionado solo al botón activo
+                    boton.classList.add('seleccionado');
+
+                    // Expande las imágenes del rol seleccionado
+                    contenedoresHeroesFooter.forEach(contenedor => {
+                        if (contenedor.dataset.rol === rol) {
+
+                            // Activa el contenedor
+                            contenedor.classList.add('active');
+
+                            // Activa las imágenes dentro del contenedor con un pequeño retraso
+                            const imagenes = contenedor.querySelectorAll(
+                                '.imagen_heroe_footer');
+                            setTimeout(() => {
+                                    imagenes.forEach(img => img.classList.add(
+                                        'active'));
+                                },
+                                200
+                            ); // Tiempo en milisegundos para que las imágenes aparezcan después del contenedor
+                        } else {
+                            // Resetea el contenedor si es otro rol
+                            contenedor.classList.remove('active');
+
+                            // También resetea las imágenes del contenedor
+                            const imagenes = contenedor.querySelectorAll(
+                                '.imagen_heroe_footer');
+                            imagenes.forEach(img => img.classList.remove('active'));
+                        }
+                    });
+                });
+            });
+
             // Cerrar sesión
             btnCerrar.onclick = function() {
                 fetch('/tierlist-maker/logout', {
@@ -397,6 +655,7 @@
                     }
                 }
             });
+
         });
     </script>
 </body>
